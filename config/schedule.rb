dbe6291ec44cb436170c3849933a61cd95e98f7c
ns-rokuyon <ness3psi@gmail.com>
2015-06-11 22:37:24 +0900

    add PATH to schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+env :PATH, ENV['PATH']
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
@@ -20,5 +21,5 @@
 # Learn more: http://github.com/javan/whenever
 
 every 1.hours do
-    command "cd /home/ns64/git_repos/eamu_images && bundle exec ruby crawler.rb --config config/crawler/wiiu.yaml"
+    command "cd /home/ns64/git_repos/eamu_images && /usr/local/bin/ruby crawler.rb --config config/crawler/wiiu.yaml >> log/wiiu_download.log 2>&1"
 end
